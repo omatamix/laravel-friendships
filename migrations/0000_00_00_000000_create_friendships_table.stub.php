d4e7ee6f3faac8e45c2d19a42f443b746125dad8
omatamix <47761650+omatamix@users.noreply.github.com>
2020-08-09 03:40:08 -0400

    Fix syntax.

--- migrations/0000_00_00_000000_create_friendships_table.stub.php
+++ migrations/0000_00_00_000000_create_friendships_table.stub.php
@@ -14,7 +14,7 @@ class CreateFriendshipsTable extends Migration
     public function up()
     {
         Schema::create(config('friendships.tables.fr_pivot'), function (Blueprint $table) {
-            $table->bigIncrements('id')
+            $table->bigIncrements('id');
             $table->morphs('sender');
             $table->morphs('recipient');
             $table->tinyInteger('status')->default(0);
